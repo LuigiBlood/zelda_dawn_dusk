4f50370e8b7808ff477cf0dac325b01ef4caf1b4
LuigiBlood <luigiblood@gmail.com>
2019-08-08 13:11:21 +0200

    Add more files to clean

--- _build_JPN_D64.bat
+++ _build_JPN_D64.bat
@@ -1,1 +1,3 @@
+IF EXIST "EZLJ_DawnDusk.d64" del "EZLJ_DawnDusk.d64"
+IF EXIST "EZLJ_DawnDusk.rom" del "EZLJ_DawnDusk.rom"
 bass.exe ./asm/EZLJ_DISK_Main.asm -d D64
--- _build_USA_D64.bat
+++ _build_USA_D64.bat
@@ -1,1 +1,3 @@
+IF EXIST "EZLE_DawnDusk.d64" del "EZLE_DawnDusk.d64"
+IF EXIST "EZLE_DawnDusk.rom" del "EZLE_DawnDusk.rom"
 bass.exe ./asm/EZLJ_DISK_Main.asm -d D64 -d USA
